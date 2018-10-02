6a9c32947a991e4bf312d107bbd81771ee35e037
Jimmy Acuña <b50060@ecci.ucr.ac.cr>
2018-10-02 14:44:42 -0600

    hasta el punto 4

--- LAB3.sql
+++ LAB3.sql
@@ -4,6 +4,8 @@
 
 use DB_B50060;
 
+--Punto 3:
+
 Create Table Estudiante(
 Cedula char(9) PRIMARY KEY,
 Email varchar(20),
@@ -34,9 +36,10 @@ Oficina char(5)
 );
 
 Create Table Asistente(
-Cedula char(9) primary key FOREIGN KEY (Cedula) REFERENCES Estudiante,
+Cedula char(9) primary key FOREIGN KEY (Cedula) REFERENCES Estudiante on delete cascade,
 NumHoras int 
 );
+
 
 Create table Curso(
 Sigla char(7) PRIMARY KEY,
@@ -50,11 +53,12 @@ SiglaCurso char(7) not null FOREIGN KEY (SiglaCurso) references Curso,
  Semestre int check (1 <= Semestre  and semestre >= 2),
  Año int
  primary key(SiglaCurso,NumGrupo,Semestre,Año),
- CedProf char(9) foreign key (CedProf) references Profesor,
+ CedProf char(9) foreign key (CedProf) references Profesor on update cascade not null,
  Carga int,
- CedAsistente char(9) foreign key (CedAsistente) references Asistente
+ CedAsistente char(9) foreign key (CedAsistente) references Asistente on delete no action
 );
 
+drop table Lleva
 Create table Lleva (
 CedEstudiante char(9) foreign key (CedEstudiante) references Estudiante,
 SiglaCurso char(7),
@@ -65,6 +69,7 @@ primary key(CedEstudiante,SiglaCurso,NumGrupo, Semestre, Año),
 foreign key(SiglaCurso,NumGrupo, Semestre, Año) references Grupo(SiglaCurso,NumGrupo,Semestre,Año)
 );
 
+
 create table Requiere_De(
 SiglaCursoRequeridor char(7) foreign key (SiglaCursoRequeridor) references Curso,
 SiglaCursoRequisito char(7) foreign key (SiglaCursoRequisito) references Curso,
@@ -72,7 +77,23 @@ primary key(SiglaCursoRequeridor,SiglaCursoRequisito)
 );
 
 create table Carrera(
-Codigo varchar(10) primary key,
+Codigo varchar(10) constraint PKCodigo primary key,
 Nombre varchar(15),
 AñoCreacion int default 1995
 );
+
+
+create table Pertence_A(
+SiglaCurso char(7) constraint FKSiglaCurso Foreign key (SiglaCurso) references Curso,
+CodCarrera varchar(10) constraint FKCodigoCarrera Foreign key (CodCarrera) references Carrera,
+NivelPlanEstudios varchar(15),
+primary key(SiglaCurso,CodCarrera)
+);
+
+----------------------------------------------------------------------------------------------------------
+
+--punto 4:
+
+insert into Estudiante
+values ('111222333', 'josef@gmail.com', 'Josefo', 'Martinez','Gauss',
+'m','1991-11-21', null, '88118811' , 'B01991', 'Grad');
