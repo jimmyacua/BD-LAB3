ff3713244380320b86c51494904293996d536727
Jimmy <jimmyacua@hotmail.com>
2018-10-05 11:11:02 -0600

    parte 7 a medias

--- LAB3.sql
+++ LAB3.sql
@@ -6,6 +6,7 @@ use DB_B50060;
 
 --Punto 3:
 
+
 Create Table Estudiante(
 Cedula char(9) PRIMARY KEY,
 Email varchar(20),
@@ -36,8 +37,9 @@ Oficina char(5)
 );
 
 Create Table Asistente(
-Cedula char(9) primary key FOREIGN KEY (Cedula) REFERENCES Estudiante on delete cascade,
-NumHoras int 
+Cedula char(9) FOREIGN KEY (Cedula) REFERENCES Estudiante (Cedula) on delete cascade,
+NumHoras int,
+primary key (Cedula)
 );
 
 
@@ -48,19 +50,20 @@ Créditos int
 );
 
 Create table Grupo(
-SiglaCurso char(7) not null FOREIGN KEY (SiglaCurso) references Curso,
+SiglaCurso char(7) FOREIGN KEY (SiglaCurso) references Curso (Sigla) on delete no action,
  NumGrupo int,
  Semestre int check (1 <= Semestre  and semestre >= 2),
  Año int
  primary key(SiglaCurso,NumGrupo,Semestre,Año),
  CedProf char(9) foreign key (CedProf) references Profesor on update cascade not null,
  Carga int,
- CedAsistente char(9) foreign key (CedAsistente) references Asistente on delete no action
+ CedAsistente char(9) foreign key (CedAsistente) references Asistente (Cedula)
 );
 
-drop table Lleva
+
 Create table Lleva (
-CedEstudiante char(9) foreign key (CedEstudiante) references Estudiante,
+CedEstudiante char(9) constraint FKCedEstudiante
+ foreign key (CedEstudiante)  references Estudiante (Cedula),
 SiglaCurso char(7),
 NumGrupo int,
 Semestre int check (1 <= Semestre  and Semestre >= 2),
@@ -71,8 +74,8 @@ foreign key(SiglaCurso,NumGrupo, Semestre, Año) references Grupo(SiglaCurso,NumG
 
 
 create table Requiere_De(
-SiglaCursoRequeridor char(7) foreign key (SiglaCursoRequeridor) references Curso,
-SiglaCursoRequisito char(7) foreign key (SiglaCursoRequisito) references Curso,
+SiglaCursoRequeridor char(7) foreign key (SiglaCursoRequeridor) references Curso (Sigla),
+SiglaCursoRequisito char(7) foreign key (SiglaCursoRequisito) references Curso (Sigla),
 primary key(SiglaCursoRequeridor,SiglaCursoRequisito)
 );
 
@@ -82,18 +85,170 @@ Nombre varchar(15),
 AñoCreacion int default 1995
 );
 
-
 create table Pertence_A(
-SiglaCurso char(7) constraint FKSiglaCurso Foreign key (SiglaCurso) references Curso,
+SiglaCurso char(7) constraint FKSiglaCurso Foreign key (SiglaCurso) references Curso (Sigla),
 CodCarrera varchar(10) constraint FKCodigoCarrera Foreign key (CodCarrera) references Carrera,
 NivelPlanEstudios varchar(15),
 primary key(SiglaCurso,CodCarrera)
 );
 
+create table Empadronado_En(
+CedEstudiante char(9) constraint FKCedEst foreign key (CedEstudiante) references Estudiante (Cedula),
+CodCarrera varchar(10) foreign key (CodCarrera) references Carrera,
+FechaIngreso date,
+FechaGraducación date,
+primary key(CedEstudiante,CodCarrera)
+);
+
+
+create table Comisión(
+Nombre varchar(15) primary key,
+Tipo varchar(15),
+Observ varchar(20)
+);
+
+
+create table Asignado_A(
+cedProf char(9) constraint CedProf foreign key (cedProf) references Profesor ON UPDATE CASCADE,
+NombComision varchar(15) foreign key (NombComision) references Comisión,
+Puesto varchar(15),
+Carga int
+);
+
+---------------------------------------------------------------------------------------
+--Lab 4: omitir en la revisión del lab 3.
+
+CREATE TABLE dbo.[Usuarios]
+(
+cedulaUsuario CHAR (9) NOT NULL ,
+nombreUsuario NVARCHAR ( 40 ) NOT NULL PRIMARY KEY ,
+PasswordHash BINARY ( 64 ) NOT NULL ,
+salt UNIQUEIDENTIFIER,
+FOREIGN KEY (cedulaUsuario) REFERENCES ESTUDIANTE (cedula)
+);
 ----------------------------------------------------------------------------------------------------------
 
 --punto 4:
 
 insert into Estudiante
 values ('111222333', 'josef@gmail.com', 'Josefo', 'Martinez','Gauss',
-'m','1991-11-21', null, '88118811' , 'B01991', 'Grad');
+'m','1991-11-21', null, '88118811' , 'B01991', 'Graduado');
+insert into Estudiante
+values ('99888777', 'anrz@gmail.com', 'Antonia', 'Rodriguez','Perez',
+'f','1994-07-1', null, '84634210' , 'B28731', 'Activo');
+
+insert into Estudiante
+values ('176543219', 'esteb@gmail.com', 'Esteban', 'Madrigal','Soto',
+'m','1997-05-29', null, '8901234' , 'B56091', 'Activo');
+insert into Estudiante
+values ('876543219', 'jen@gmail.com', 'Jenny', 'Cascante','Castro',
+'f','1999-03-19', null, '88664422' , 'B71354', 'Activo');
+
+
+
+insert into Profesor
+values('123456789','alexm@gmail.com', 'Alexandra', 'Martinez',null, 'F', '1986-10-14',
+null, '87654321', 'Asociada', '2008-01-01', 'IF502');
+insert into Profesor
+values('187654321','chiki@gmail.com', 'Francisco', 'Arroyo',null, 'M', '1960-06-23',
+null, '86543210', 'Catedratico', '2005-08-13', 'IF210');
+insert into Profesor
+values('234567890','skikut@gmail.com', 'Sandra', 'Kikut','Valverde', 'F', '1971-01-10',
+null, '85437121', 'Catedratico', '2008-06-01', 'IF212');
+
+insert into Asistente
+values('111222333', 9);
+insert into Asistente
+values('99888777', 5);
+
+
+insert into Curso 
+values('ci1310', 'Sistemas Operativos', 4);
+insert into Curso 
+values('ci1312', 'Bases de Datos 1', 4);
+insert into Curso 
+values('ci1221', 'Estructuras de datos', 4);
+
+
+insert into Grupo
+values('ci1310', 1, 2, 2018, '187654321',4, '111222333');
+insert into Grupo
+values('ci1312', 1, 2, 2018, '123456789',4, '99888777');
+
+
+insert into Lleva 
+values('176543219','ci1310', 1, 2,2018);
+insert into Lleva 
+values('876543219','ci1312', 1, 2,2018);
+
+
+insert into Requiere_De 
+values('ci1310','ci1221');
+insert into Requiere_De 
+values('ci1312','ci1221');
+
+insert into Carrera 
+values('420705','Computación', 1960);
+insert into Carrera 
+values('420201','Ing Electrica', 1965);
+insert into Carrera 
+values('000010','Historia', 2001);
+
+select * from Carrera;
+
+--5:
+update Carrera
+set AñoCreacion = '1975'
+where Codigo = '420705';
+
+--6:
+delete from Carrera
+where Codigo = '000010';
+
+--7:
+
+--7.a:
+
+-- delete cascade
+select g.CedAsistente
+from Asistente a join Grupo g on g.CedAsistente = a.Cedula;
+
+--delete no action
+select g.SiglaCurso
+from Grupo g join Curso c on c.Sigla = g.SiglaCurso;
+
+--update cascade
+select p.Cedula
+from Grupo g join Profesor p on p.Cedula = g.CedProf;
+
+--7.b:
+select g.CedAsistente, a.Cedula
+from Asistente a join Grupo g on g.CedAsistente = a.Cedula;
+
+select c.Sigla, g.SiglaCurso
+from Grupo g join Curso c on c.Sigla = g.SiglaCurso;
+
+select p.Cedula, g.CedProf
+from Grupo g join Profesor p on p.Cedula = g.CedProf;
+
+--7.c
+delete from Estudiante
+where Cedula = '111222333';
+
+select *
+from Grupo;
+
+
+delete from Curso
+where Sigla = 'ci1312';
+
+select c.Sigla, g.SiglaCurso
+from Grupo g join Curso c on c.Sigla = g.SiglaCurso;
+
+
+update Profesor
+set Cedula = '187654300'
+where Cedula = '187654321';
+select p.Cedula, g.CedProf
+from Grupo g join Profesor p on p.Cedula = g.CedProf;
+
